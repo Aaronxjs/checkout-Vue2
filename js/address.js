5b8bac63fe5cb6f3f1b564abebed1decc6da76fd
xujianshe <1556300171@qq.com>
2018-09-20 15:53:02 +0800

    no message

--- js/address.js
+++ js/address.js
@@ -1,21 +1,44 @@
 var vm = new Vue({
 	el: ".container",
 	data: {
-		addressDataList:[]
+		addressLimitNum: 3,
+		addressDataList:[],
+		addressMoreFlag: false,
+		currentIndex: 0,
+		shippingMethod: 0
 	},
-	counted:function(){
+	mounted:function(){
 		this.$nextTick(()=>{
 			this.getAddressDataList();
 		});
+	},
+	computed: {
+		filterAddressList: function(){
+			return this.addressDataList.slice(0, this.addressLimitNum);
+		}
 	},
 	methods:{
 		getAddressDataList: function(){
 			this.$http.get('data/address.json').then(response =>{
 				var data = response.data;
-				if(data.status == 1){
+				if(data.status == 0){
 					this.addressDataList = data.result;
 				}
 			})
+		},
+		setAddressMore: function(){
+			if(!this.addressMoreFlag){
+				this.addressLimitNum = this.addressDataList.length;
+			}else{
+				this.addressLimitNum = 3;
+			}
+			this.addressMoreFlag = !this.addressMoreFlag;
+		},
+		setDefaultAddress: function(address){
+			this.addressDataList.forEach((item)=>{
+				item.isDefault = false;
+			})
+			address.isDefault = true;
 		}
 	}
 });
--- js/cart.js
+++ js/cart.js
@@ -79,3 +79,4 @@ var vm = new Vue({
 		}
 	}
 });
+
